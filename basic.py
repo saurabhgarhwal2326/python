9a202c924e960ef060785b8179baeb037127d6f1
saurabh garhwal <saurabhgarhwal353@gmail.com>
2024-09-30 11:58:24 +0530

    Add files via upload
    About python

---
+++ basic.py
@@ -0,0 +1,33 @@
+# a = 10
+# a = "hello"
+# # print(a)
+# a = 55.5
+# a = True
+# print(a)
+# 4 = 5 
+# print(4)
+# age = 22
+# Age = 23
+# print(age)
+# a = 10
+# a = 4,3,3,4,5,6,6,6,6,6,6,6,7
+# print(a)
+# tuple 
+# list 
+# string 
+# operation on string 
+# name = "Sourbh"
+# print(name[0])
+# slicing in string
+# print(name[0:3])
+# print(len(name))
+# last_name = "Garhwal"
+# a=name+" "+last_name
+# print(a)
+# b=a.find("G")
+# print(b)
+# uppercase=a.upper()
+# print(uppercase)
+# b = "SOURBH GARHWAL"
+# lower_case =b.lower()
+# print(lower_case)
